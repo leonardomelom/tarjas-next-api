4bf09879f8b7b01f3c7a73a6933be4e71fe4bc3d
Leonardo melo <sempitern4l.01@gmail.com>
2022-12-08 16:37:49 -0300

    next routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri:"https://api-sa-east-1.graphcms.com/v2/cl4ozypzy1h2801xscbvd4dc7/master",
+
+  cache: new InMemoryCache(),
+});
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+  <ApolloProvider client={client}>
+  <Component {...pageProps} />
+  </ApolloProvider>
+)}
 
 export default MyApp
---
+++ pages/api/promedicina.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const fs = require('fs/promises')
+const path = require('path')
+const process = require('process')
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req
+
+  if (method === 'GET') {
+    try {
+      const payload = await fs
+        .readFile(path.join(process.cwd(), 'promed.json'), 'utf8')
+        .then(JSON.parse)
+
+      res.status(200).json(payload)
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  } else if (method === 'PUT') {
+    try {
+      await fs.writeFile(
+        path.join(process.cwd(), 'promed.json'),
+        JSON.stringify(req.body)
+      )
+
+      res.status(200).json({ message: 'Dados modificados com sucesso' })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  }
+
+  return res.status(404).json({ message: 'Route note found.' })
+}
