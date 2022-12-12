7d8f767e9f717b1d66dca3794695a68df1915b97
Leonardo melo <sempitern4l.01@gmail.com>
2022-12-12 14:05:55 -0300

    hotfix

--- pages/api/promedicina.ts
+++ pages/api/promedicina.ts
@@ -16,9 +16,9 @@ export default async function handler(
         .readFile(path.join(process.cwd(), 'promed.json'), 'utf8')
         .then(JSON.parse)
 
-      res.status(200).json(payload)
+      return res.status(200).json(payload)
     } catch (error) {
-      res.status(500).json({ message: 'Internal Server Error' })
+      return res.status(500).json({ message: 'Internal Server Error' })
     }
   }
   if (method === 'PUT') {
@@ -28,9 +28,9 @@ export default async function handler(
         JSON.stringify(req.body)
       )
 
-      res.status(200).json({ message: 'Dados modificados com sucesso' })
+      return res.status(200).json({ message: 'Dados modificados com sucesso' })
     } catch (error) {
-      res.status(500).json({ message: 'Internal Server Error' })
+      return res.status(500).json({ message: 'Internal Server Error' })
     }
   }
 
